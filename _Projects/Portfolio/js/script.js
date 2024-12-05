ee4ab853e599eb96f1ac857bcc3b9e9ba777e7fd
benjaminhuang <benjaminhuanghuang@gmail.com>
2024-12-04 23:45:22 -0800

    portfolio

--- _Projects/Portfolio/js/script.js
+++ _Projects/Portfolio/js/script.js
@@ -4,3 +4,25 @@
  */
 
 "use strict";
+
+/**
+ * Light and dark mode toggle
+ */
+const $HTML = document.documentElement;
+const $themeBtn = document.querySelector("[data-theme-btn]"); 
+let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if(sessionStorage.getItem("theme") === "dark") {
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  $HTML.dataset.theme = isDark ? "dark" : "light";
+  sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+const changeTheme = () => {
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "dark" ? "light" : "dark";
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+};
+
+$themeBtn.addEventListener("click", changeTheme)
+ 
