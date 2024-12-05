4669aa4649614587e7c1e4d0ef448c18291cf609
benjaminhuang <benjaminhuanghuang@gmail.com>
2024-12-05 10:03:48 -0800

    portfolio

--- _Projects/Portfolio/js/script.js
+++ _Projects/Portfolio/js/script.js
@@ -26,3 +26,21 @@ const changeTheme = () => {
 
 $themeBtn.addEventListener("click", changeTheme)
  
+/**
+ * TAB
+ */
+const $tabBtns = document.querySelectorAll("[data-tab-btn]");
+let [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let [lastActiveTabBtn] = $tabBtns;
+
+$tabBtns.forEach(item => {
+    item.addEventListener("click", function() {
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+        const $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this;
+    });
+});
