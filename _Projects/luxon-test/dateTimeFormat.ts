ad389b554fd60619f3c297324b1e3ebd6c58f91e
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-01-09 12:37:02 -0800

    update

---
+++ _Projects/luxon-test/dateTimeFormat.ts
@@ -0,0 +1,8 @@
+import { DateTime } from 'luxon';
+
+
+export type RelativeDate = 'Today' | "Upcoming" | "This Week" | "This Year" | "Past" | "Can Not ";
+export function formatTime(dateTime) {
+    
+
+}
